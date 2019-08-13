07a6de586326d5aaa70231e8a4e41f653696b2ba
Fabrice Etiennette <fabrice.etiennette@hotmail.com>
2019-08-13 18:56:04 +0200

    comment added

--- Instagrid/UIView+.swift
+++ Instagrid/UIView+.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 extension UIView {
-    
+    // This animation is use to make more fluid the transition between frames
     func flashAnimation() {
         let flash = CABasicAnimation(keyPath: "opacity")
         
@@ -21,13 +21,14 @@ extension UIView {
         flash.autoreverses = true
         layer.add(flash, forKey: nil)
     }
-
+    // This animation is use to move an element from a position to another
     func animateAndMove(x: CGFloat,y: CGFloat) {
         UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 1.5, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
             self.transform = CGAffineTransform(translationX: x, y: y)
         })
     }
     
+    // This animation is use to bring back an element from a position to another. It also use device orientation
     func animateBack(x: CGFloat, y: CGFloat, delay: Double) {
         if UIDevice.current.orientation.isLandscape {
             self.transform = CGAffineTransform(translationX: x, y: y)
