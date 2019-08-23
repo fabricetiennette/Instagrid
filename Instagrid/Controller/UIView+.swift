baedb5d5b91d0b3cd1af12037919af14bcb4f779
Fabrice Etiennette <fabrice.etiennette@hotmail.com>
2019-08-23 19:28:04 +0200

    additionel comment added

--- Instagrid/Controller/UIView+.swift
+++ Instagrid/Controller/UIView+.swift
@@ -20,14 +20,14 @@ extension UIView {
         flash.autoreverses = true
         layer.add(flash, forKey: nil)
     }
-    // This animation is use to move an element from a position to another
+    /// This animation is use to move an element from a position to another
     func animateAndMove(x: CGFloat,y: CGFloat) {
         UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 1.5, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
             self.transform = CGAffineTransform(translationX: x, y: y)
         })
     }
     
-    // This animation is use to bring back an element from a position to another. It also use device orientation
+    /// This animation is use to bring back an element from a position to another. It also use device orientation
     func animateBack(x: CGFloat, y: CGFloat, delay: Double) {
         if UIDevice.current.orientation.isLandscape {
             self.transform = CGAffineTransform(translationX: x, y: y)
